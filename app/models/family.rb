d1e864cba23752f83455d5e6d57c1de02d8bbdec
Duarte Henriques <duarte.henriques@gmail.com>
2017-12-07 19:30:06 +0000

    form poc

--- app/models/family.rb
+++ app/models/family.rb
@@ -4,10 +4,12 @@ class Family < ApplicationRecord
 
   # Allows active_admin to submit json as text and have it stored as json
   def info=(value)
-    value = begin
-      JSON.parse(value)
-    rescue JSON::ParserError
-      value
+    if value.is_a?(String)
+      value = begin
+        JSON.parse(value)
+      rescue JSON::ParserError
+        value
+      end
     end
     super(value)
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,12 @@ class Product < ApplicationRecord
 
   # Allows active_admin to submit json as text and have it stored as json
   def info=(value)
-    value = begin
-      JSON.parse(value)
-    rescue JSON::ParserError
-      value
+    if value.is_a?(String)
+      value = begin
+        JSON.parse(value)
+      rescue JSON::ParserError
+        value
+      end
     end
     super(value)
   end
